a4079138cb4b4a0a8de0196d885aa851a2d0266c
Kousuke2221 <165615897+Kousuke2221@users.noreply.github.com>
2024-04-19 14:45:31 +0900

    iroiro

--- Src/Scene/SceneResult.cpp
+++ Src/Scene/SceneResult.cpp
@@ -6,7 +6,11 @@
 int R_Result_Score_Handle;
 int R_Result_Player1_WIN_Handle;
 int R_Result_Player2_WIN_Handle;
-int R_Result_Win_Score_Handle;
+int R_Result_Selectino_Handle[2];
+int R_Result_Right_Handle;
+int R_Result_Left_Handle;
+int R_Result_Frame_Handle;
+
 
 int R_ResultPoxX; //座標
 
@@ -14,9 +18,9 @@ bool R_Continue;	//コンテニューフラグ
 
 bool R_BackTitle;	//タイトルフラグ
 
-bool R_Wins_Score;
+bool R_Wins_Score; //画像切り替え
 
-int images[RLSULT]; // 画像を格納する配列
+int images[WIN_RLSULT]; // 画像を格納する配列
 int currentImageIndex = 0; // 現在の画像のインデックス
 
 //タイトル初期化
@@ -25,20 +29,25 @@ void Result::InitResult()
 	R_Result_Score_Handle = LoadGraph(RISULT_SCORE);
 	R_Result_Player1_WIN_Handle = LoadGraph(RISULT_PLAYER_1_WIN);
 	R_Result_Player2_WIN_Handle = LoadGraph(RISULT_PLAYER_2_WIN);
-	R_Result_Win_Score_Handle = LoadGraph(RISULT_WIN_SCORE);
-
-	// すべての画像パスを配列にまとめる
-	const char* image_paths[RLSULT] = {
+	R_Result_Selectino_Handle[0] = LoadGraph(RISULT_WIN_SCORE);
+	R_Result_Selectino_Handle[1] = LoadGraph(RISULT_SELECTION);;
+	R_Result_Right_Handle = LoadGraph(RISULT_RIGHT);;
+    R_Result_Left_Handle = LoadGraph(RISULT_LEFT);;
+	R_Result_Frame_Handle = LoadGraph(RISULT_FRAME);;
+	
+	const char* image_paths[WIN_RLSULT] = {
 		RISULT_PLAYER_1_WIN,
 		RISULT_PLAYER_2_WIN,
-		RISULT_WIN_SCORE,
 		RISULT_SCORE
 	};
 	R_ResultPoxX = 150;
 
+	R_Wins_Score = false;//画像切り替え
+
 	R_Continue = false;		//コンテニューフラグを折る
 
 	R_BackTitle = false;	//タイトルフラグを折る
+
 
 	//リザルトループシーンへ移動
 	g_CurrentSceneID = SCENE_ID_LOOP_RESULT;
@@ -47,48 +56,49 @@ void Result::InitResult()
 //タイトル通常処理
 void Result::StepResult()
 {
-
-	//Enterキーを押す
-	if (IsKeyPush(KEY_INPUT_A) || IsKeyPush(KEY_INPUT_LEFT))	//Aキーまたは左矢印キーが押されたら
-	{
-		if (R_ResultPoxX == 650)
-		{
-			R_ResultPoxX = 150;	//隣に移動する
-		}
-	}
-
-	else if (IsKeyPush(KEY_INPUT_D) || IsKeyPush(KEY_INPUT_RIGHT))	//Dキーまたは右矢印キーが押されたら
-	{
-		if (R_ResultPoxX == 150)
-		{
-			R_ResultPoxX = 650;	//隣に移動する
-		}
-	}
-
-	else if (IsKeyPush(KEY_INPUT_RETURN))	//エンターキーを押すと
-	{
-		if (R_ResultPoxX == 150)
-		{
-			R_Continue = true;	//コンテニューフラグを立てる
-		}
-
-		else if (R_ResultPoxX == 650)
-		{
-			R_BackTitle = true;	//タイトルフラグを折る
-		}
 	
+		if (IsKeyPush(KEY_INPUT_A) || IsKeyPush(KEY_INPUT_LEFT))	//Aキーまたは左矢印キーが押されたら
+		{
+			if (R_ResultPoxX == 650)
+			{
+				R_ResultPoxX = 150;	//隣に移動する
+			}
+		}
+
+		else if (IsKeyPush(KEY_INPUT_D) || IsKeyPush(KEY_INPUT_RIGHT))	//Dキーまたは右矢印キーが押されたら
+		{
+			if (R_ResultPoxX == 150)
+			{
+				R_ResultPoxX = 650;	//隣に移動する
+			}
+		}
+
+		else if (IsKeyPush(KEY_INPUT_RETURN))	//エンターキーを押すと
+		{
+			if (R_ResultPoxX == 150)
+			{
+				R_Continue = true;	//コンテニューフラグを立てる
+			}
+
+			else if (R_ResultPoxX == 650)
+			{
+				R_BackTitle = true;	//タイトルフラグを折る
+			}
+
 			//リザルト後処理シーンへ移動
 			g_CurrentSceneID = SCENE_ID_FIN_RESULT;
-		
-	}
+
+		}
+	
 }
 
 //タイトル描画処理
 	void Result::DrawResult()
 {
-	DrawString(0, 0, "リザルトシーンです", GetColor(255, 255, 255));
+	DrawGraph(0, 0, R_Result_Selectino_Handle[0], true);
 	DrawGraph(0, 0, R_Result_Score_Handle, true);
-
+	DrawString(0, 0, "リザルトシーンです", GetColor(255, 255, 255));
+	
 }
 
 //タイトル後処理
--- Src/Scene/SceneResult.h
+++ Src/Scene/SceneResult.h
@@ -3,9 +3,12 @@
 #define RISULT_SCORE "Data/ClearImage/kari_Draw.png"
 #define RISULT_PLAYER_1_WIN "Data/ClearImage/kari_Player1win.png"
 #define RISULT_PLAYER_2_WIN  "Data/ClearImage/kari_Player2win.png"
-#define RISULT_WIN_SCORE "Data/ClearImage/kari_result"
-
-#define RLSULT 4
+#define RISULT_WIN_SCORE "Data/ClearImage/kari_result.png"
+#define RISULT_SELECTION "Data/ClearImage/result_selection.png"
+#define RISULT_RIGHT "Data/ClearImage/result_selection(1).png"
+#define RISULT_LEFT "Data/ClearImage/result_selection(2).png"
+#define RISULT_FRAME "Data/ClearImage/result_selection(3).png"
+#define WIN_RLSULT 3
 class Result
 {
 private:
