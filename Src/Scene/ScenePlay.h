9c0afb4052b167b58b5290ed58fe6361e39f879d
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-20 17:34:50 +0900

    2つ目のゲージ改良中と判定画像描画

--- Src/Scene/ScenePlay.h
+++ Src/Scene/ScenePlay.h
@@ -2,13 +2,18 @@
 
 
 #define BG_HUNDLE_PATH			"Data/Playimage/karibackground2.png"	//背景画像
+#define FEALD_HUNDLE_PATH		"Data/Playimage/background.png"			//フィールド画像
+
+
 #define GAUGE_FLAMEHUNDLE_PATH	"Data/Playimage/PowerBox.png"			//ゲージ(外枠)画像
 #define GAUGE_HUNDLE_PATH		"Data/Playimage/gauge.png"				//ゲージ(本体)画像
+#define GAUGE_HEIGHT		(200)										//ゲージの縦幅
 
-#define GAUGE2_HUNDLE_PATH		"Data/Playimage/Charge.png"				//ゲージ2つ目
-#define GAUGE2_1_HUNDLE_PATH	"Data/Playimage/Chargebox_arrow.png"	//矢印
+#define GAUGE2_HUNDLE_PATH		"Data/Playimage/Charge.png"				//サイドゲージ画像
+#define GAUGE2_1_HUNDLE_PATH	"Data/Playimage/Chargebox_arrow.png"	//矢印画像
+#define SIDEGAUGE_WIDHT		(300)										//サイドゲージの横幅
 
-#define ANIME_MAX_NUM		(3)
+#define ANIME_MAX_NUM		(3)											//アニメ枚数
 #define PLAYER_WAIT_PATH		"Data/Playimage/Player_wait1.png"		//プレイヤー画像(待機)
 #define PLAYER_MOVE_PATH		"Data/Playimage/player_move.png"		//プレイヤー画像(モーション)
 #define PLAYER_ATTACK_PATH		"Data/Playimage/Player_attack.png"		//プレイヤー画像(攻撃)
@@ -18,42 +23,54 @@
 #define CPU_MOVE_PATH			"Data/Playimage/CPU_wait1.png"			//CPU画像(モーション)
 #define CPU_ATTACK_PATH			"Data/Playimage/CPU_wait1.png"			//CPU画像(攻撃)
 
-#define FEALD_HUNDLE_PATH		"Data/Playimage/background.png"			//フィールド画像
+#define GOOD_PATH				"Data/Playimage/Good.png"				//Good画像
+#define GREAT_PAYH				"Data/Playimage/Greatpng.png"			//Great画像
+#define PERFECT_PATH			"Data/Playimage/Perfect.png"			//Perfect画像
 
-#define GAUGE_HEIGHT		(200)
 
-//画像構造体
-struct Hundl
-{
-	int BgHndl;			//背景画像ハンドル
-	int GaugeFlameHndl;	//ゲージ(外枠)ハンドル
-	int GaugeHndl;		//ゲージ(本体)ハンドル
-	int SideGaugeHndl;	//サイドのゲージ
-	int SideSelectHndl;	//動かすほう	
-	int FealdHndl;		//フィールドハンドル
-	int CPC_Hndl[ANIME_MAX_NUM];		//CPUハンドル
-	int PlayerHndl[2][ANIME_MAX_NUM];	//プレイヤーハンドル
-
-	
-};
 
 //プレイの状態管理
-enum PlayState
+enum PLAYSTATE
 {
 	State_SetPower = 0,		//パワーを決める状態
-	State_SetPoint,		//瓦を割る位置決定状態
-	State_Break,		//瓦を割る状態
+	State_SetPoint,			//瓦を割る位置決定状態
+	State_Break,			//瓦を割る状態
 
-	State_Num			//状態の数
+	State_Num				//状態の数
 };
 
+//ゲージの状態管理
+enum GAUGESTATE
+{
+	State_Good = 0,		//Good
+	State_Great,		//Great
+	State_Perfect,		//Perfect
+
+	Stete_Judge_Num		//ゲージの状態の数
+};
+
+//画像構造体
+struct HUNDL
+{
+	int BgHndl;							//背景画像ハンドル
+	int FealdHndl;						//フィールド1画像ハンドル
+	int GaugeFlameHndl;					//ゲージ(外枠)画像ハンドル
+	int GaugeHndl;						//ゲージ(本体)画像ハンドル
+	int SideGaugeHndl;					//サイドゲージ画像ゲージ
+	int SideSelectHndl;					//矢印画像ハンドル	
+	int JudgeHndl[Stete_Judge_Num];		//判定画像ハンドル
+
+	int CPC_Hndl[ANIME_MAX_NUM];		//CPU画像ハンドル
+	int PlayerHndl[2][ANIME_MAX_NUM];	//プレイヤー画像ハンドル
+
+};
 
 class Play
 {
 private:
-	Hundl Hndl;
-
-	PlayState playState;
+	HUNDL Hndl;
+	PLAYSTATE playState;
+	GAUGESTATE GaugeState;
 
 	int FlameCount;			//フレームカウント
 	float Dely;				//ディレイ用変数
@@ -72,6 +89,8 @@ private:
 	float SideGaugeVolume;	//サイドゲージの増加量
 	bool IsSideGauge;		//サイドゲージ実行中フラグ
 	int PosX, PosY;			//動くゲージの座標格納変数
+	bool IsRight;			//右に動くか
+	bool IsLeft;			//左に動くか
 	int i;					//サイドゲージのswith分変数
 	/*======2つ目のゲージに関する変数========*/
 
@@ -96,11 +115,12 @@ public:
 	//画像読み込み関数
 	void LoadHundl();
 
-	//
-
 	//ゲージ増減関数
 	void GaugeUpDown();
 
-	//サイドゲージ移動と増減関数
+	//サイドゲージ増減関数
+	void SideGaugeUpDown();
+
+	//サイドゲージ移動関数
 	void SideGauge();
 };
